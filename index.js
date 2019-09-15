b1c9cdd541b53d0d9799daccf6d3b635bc350559
Captainfive <vinou_montes@hotmail.fr>
2019-09-15 03:58:34 +0200

    chore(index): adding server and post and get endpoint

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 "use strict";
+
+const polka = require("polka");
+const bodyParser = require("body-parser");
+
+// create application/json parser
+const jsonParser = bodyParser.json();
+
+/**
+ * @async
+ * @function main
+ * @returns {Promise<void>}
+ */
+async function main() {
+    polka()
+        .get("/users", (req, res) => {
+            console.log(`Hello`);
+            res.end(`User: ${req.params.id}`);
+        })
+        .post("/users/post", jsonParser, (req, res) => {
+            console.log(req.body);
+        })
+        .listen(1337, (err) => {
+            if (err) {
+                throw err;
+            }
+            console.log("> Running on localhost:1337");
+        });
+}
+main().catch(console.error);

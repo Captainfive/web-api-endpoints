263dc4c6ee0bd5b68d2071485f540307f76ad230
Captainfive <vinou_montes@hotmail.fr>
2019-09-20 04:35:02 +0200

    chore(data): repo created for private test

--- index.js
+++ index.js
@@ -1,30 +1,49 @@
 "use strict";
+require("make-promises-safe");
 
+// Require NodeJs dependencies
+const { readFile } = require("fs").promises;
+const { join } = require("path");
+
+// Require third party dependencies
 const polka = require("polka");
 const bodyParser = require("body-parser");
 
 // create application/json parser
 const jsonParser = bodyParser.json();
 
-/**
- * @async
- * @function main
- * @returns {Promise<void>}
- */
-async function main() {
-    polka()
-        .get("/users", (req, res) => {
-            console.log(`Hello`);
-            res.end(`User: ${req.params.id}`);
-        })
-        .post("/users/post", jsonParser, (req, res) => {
-            console.log(req.body);
-        })
-        .listen(1337, (err) => {
-            if (err) {
-                throw err;
-            }
-            console.log("> Running on localhost:1337");
-        });
-}
-main().catch(console.error);
+// Json example path
+const jsonExample = join(__dirname, "data", "addons.json");
+
+polka()
+    .use(bodyParser.json())
+    .get("/addons", async(req, res) => {
+        // Read our json exemple.json
+        const readJson = await readFile(jsonExample);
+
+        res.end(readJson);
+    })
+    .get("/addons/:name", async(req, res) => {
+        const { name } = req.params;
+        console.log(name);
+        // Read our json exemple.json
+        const readJson = await readFile(jsonExample);
+        console.log(readJson);
+
+        res.end(JSON.parse(readJson));
+    })
+    .post("/post/addons", jsonParser, (req, res) => {
+        const { body, headers: { "content-type": contentType } } = req;
+
+        if (contentType !== "application/json") {
+            console.log("wrong format");
+        }
+
+        res.end();
+    })
+    .listen(1993, (err) => {
+        if (err) {
+            throw err;
+        }
+        console.log("> Running on localhost:1993");
+    });
